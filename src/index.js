62f8d7bce8193d7caab43abeafd72299b9bbb3eb
khtulu <khtulu1512@gmail.com>
2021-01-27 22:46:38 +0300

    lesson 10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello');
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './css/style.css';
+
+render(<App />, document.querySelector('#root'));
